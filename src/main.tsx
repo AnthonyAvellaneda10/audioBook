d824374bf1fd71e621675cf3b9b4f3f6eb963b43
Anthony <anthony.avellaneda.p@uni.pe>
2026-05-27 23:52:25 -0500

    feat(auth): install and configure react-oidc-context and add useAuth abstraction hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { AuthProvider } from "react-oidc-context";
+import { cognitoAuthConfig } from "./app/config/authConfig";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider {...cognitoAuthConfig}>
+    <App />
+  </AuthProvider>
+);
